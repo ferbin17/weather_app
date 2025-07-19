6a83f95235a8ea628609225c2bd6a965d5ca0b1a
ferbin17 <ferbin17@gmail.com>
2025-07-19 19:28:08 +0530

    Added trusted proxies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,10 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Trust all proxies so request.remote_ip uses X-Forwarded-For
+  config.action_dispatch.trusted_proxies = [
+    IPAddr.new("0.0.0.0/0"), # all IPv4
+    IPAddr.new("::/0")        # all IPv6
+  ]
 end
